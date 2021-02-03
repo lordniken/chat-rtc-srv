8feb73ab9a7c6974947ef7a9416ccb74d4310d05
Alexey Malyshev <lordniken@mail.ru>
2021-02-03 20:25:24 +0300

    Messages fetching limit

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -2,6 +2,7 @@ import { EnchWebSocket } from '../types';
 import ws = require('ws');
 const Message = require('../models/Message');
 const actions = require('../utils/actions');
+const helpers = require('../utils/helpers');
 
 exports.send = async (ws, action) => {
   const wss = import('../server');
@@ -20,16 +21,31 @@ exports.send = async (ws, action) => {
 };
 
 exports.fetch = async (ws, action) => {
-  const messages = await Message.find(
-    {
-      $or: [
-        { author: action.payload },
-        { to: action.payload }
-      ]
-    }
-  );
+  const FETCH_MESSAGES_LIMIT = 20;
+
+  const query = {
+    $or: [
+      { author: action.payload.id, to: ws.uid },
+      { author: ws.uid, to: action.payload.id }
+    ]
+  };
+
+  let limit = FETCH_MESSAGES_LIMIT;
+
+  if (action.payload.count) {
+    limit = ((action.payload.count / FETCH_MESSAGES_LIMIT) + 1) * FETCH_MESSAGES_LIMIT;
+  }
+
+  const totalMessages = await Message.find(query).countDocuments();
+  const messages = await Message.find(query).sort({ date: -1 }).limit(limit);
+  const sortedMessages = messages.sort(helpers.sortCompare('date'));
+
+  const payload = JSON.stringify({
+    list: sortedMessages,
+    totalMessages
+  });
 
   if (messages) {
-    ws.send(JSON.stringify(actions.fetchMessages(messages)));
+    ws.send(JSON.stringify(actions.fetchMessages(payload)));
   }
 };
